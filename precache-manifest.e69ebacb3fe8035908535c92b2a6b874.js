4e5ca9a778fcb5a6e99ef683daf2c29483e4e6ec
sashu-sashu <iamsashaalexandra@gmail.com>
2022-09-25 15:06:20 +0200

    Updates

---
+++ precache-manifest.e69ebacb3fe8035908535c92b2a6b874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "decac41d336c3a197d8d",
+    "url": "/meet-app/static/css/main.92371316.chunk.css"
+  },
+  {
+    "revision": "decac41d336c3a197d8d",
+    "url": "/meet-app/static/js/main.decac41d.chunk.js"
+  },
+  {
+    "revision": "38ecc02d009634cff82d",
+    "url": "/meet-app/static/js/1.38ecc02d.chunk.js"
+  },
+  {
+    "revision": "0722ac33ea8865717d33",
+    "url": "/meet-app/static/js/2.0722ac33.chunk.js"
+  },
+  {
+    "revision": "582bf03d074262eed522",
+    "url": "/meet-app/static/js/runtime~main.582bf03d.js"
+  },
+  {
+    "revision": "e5ddb621cfc2bc9d177be1a0c8757534",
+    "url": "/meet-app/index.html"
+  }
+];
